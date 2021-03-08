892facdeb9f8c7f0307507c86fab785dc399163c
forge1010 <ale.silva1120@gmail.com>
2021-03-08 16:08:18 -0300

    resolvendo bugs pt2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ export default class Mydocument extends Document {
         return(
          <Html>
              <Head>
+             <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable = no" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black" />
                  <link rel="shortcut icon" href="favicon.png" type="image/png"/>
              <link rel="preconnect" href="https://fonts.gstatic.com" />
              <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
